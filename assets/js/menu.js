46c185f5117fb3f69bdc31630b62c2359caa616f
penielpam <penielpam@gmail.com>
2024-09-23 17:27:21 +0100

    update

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,18 +1,18 @@
-if(window.location.href == "https://SolvPath.co/" || window.location.href == "https://SolvPath.co/index.html" || window.location.href == "https://SolvPath.co/index"){
+if(window.location.href == "https://solvepath.live/" || window.location.href == "https://solvepath.live/index.html" || window.location.href == "https://solvepath.live/index"){
 
     $('.navbar-nav li a').removeClass('active');
     $('#home-link').addClass('active')
 
-}else if( window.location.href == "https://SolvPath.co/about.php" || window.location.href == "https://SolvPath.co/about"){
+}else if( window.location.href == "https://solvepath.live/about.php" || window.location.href == "https://solvepath.live/about"){
     
     $('.navbar-nav li a').removeClass('active');
     $('#about-link').addClass('active')
 
-}else if( window.location.href == "https://SolvPath.co/services.php" || window.location.href == "https://SolvPath.co/services"){
+}else if( window.location.href == "https://solvepath.live/services.php" || window.location.href == "https://solvepath.live/services"){
     
     $('.navbar-nav li a').removeClass('active');
     $('#service-link').addClass('active')
-}else if( window.location.href == "https://SolvPath.co/contact.php" || window.location.href == "https://SolvPath.co/contact"){
+}else if( window.location.href == "https://solvepath.live/contact.php" || window.location.href == "https://solvepath.live/contact"){
     
     $('.navbar-nav li a').removeClass('active');
     $('#contact-link').addClass('active')
